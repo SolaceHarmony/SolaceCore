813bcee02a5ee5a739bbd68bc721eddb9dcd7412
Sydney Renee <sydney@solace.ofharmony.ai>
2025-10-14 14:49:00 -0700

    Web: migrate to ComposeViewport and opt-in; raise memory (-Xmx2g) for Kotlin JS prod compile; web build now succeeds

--- composeApp/src/webMain/kotlin/main.kt
+++ composeApp/src/webMain/kotlin/main.kt
@@ -1,11 +1,12 @@
 package org.solace.composeapp
 
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    CanvasBasedWindow(title = "SolaceCore Monitor") {
+    // Mount the app into the root element defined in src/webMain/resources/index.html
+    ComposeViewport(viewportContainerId = "root") {
         App()
     }
 }
